63fc4aa492a6c4a5a996dff36a1a2e1194fcbc02
ashley.snowdon <ashley.snowdon@94e4fe08-8463-429f-bf9a-f296408ad73b>
2011-07-20 23:02:01 +0000

    Encrypt password on save

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -2,4 +2,11 @@ class Mailbox < ActiveRecord::Base
   belongs_to :domain
 
   validates_presence_of :domain_id, :email
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password = Digest::MD5.hexdigest( self.password )
+  end
+
 end
