8a51c4cd8711d9d63d1f2d89fdda2fd2d1a14bc5
ashley.snowdon <ashley.snowdon@94e4fe08-8463-429f-bf9a-f296408ad73b>
2011-07-25 13:39:06 +0000

    Removed placeholder

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 $(function() {
   $("#mailboxes_search input").attr('autocomplete', 'off');
   $("#mailaliases_search input").attr('autocomplete', 'off');
