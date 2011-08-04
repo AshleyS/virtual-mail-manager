bde0efbfeba7d090207bb6a0c7ec439423e7241c
Ashley Snowdon <ashley@taikutsu.local>
2011-08-05 00:02:37 +0100

    Modified to use MD5-crypt

--- app/helpers/mailboxes_helper.rb
+++ app/helpers/mailboxes_helper.rb
@@ -1,2 +1,25 @@
 module MailboxesHelper
+  class Password
+    class << self
+      # Generate an MD5 salt string
+      def salt
+        seeds = ('a'..'z').to_a
+        seeds.concat( ('A'..'Z').to_a )
+        seeds.concat( (0..9).to_a )
+        seeds.concat ['/', '.']
+        seeds.compact!
+
+        salt_string = '$1$'
+        8.times { salt_string << seeds[ rand(seeds.size) ].to_s }
+        salt_string << '$'
+
+        salt_string
+      end
+
+      # Crypt a password suitable for use in shadow files
+      def crypt( string )
+        string.crypt( self.salt )
+      end
+    end
+  end
 end
--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -8,7 +8,7 @@ class Mailbox < ActiveRecord::Base
   before_save :encrypt_password
 
   def encrypt_password
-    self.password = Digest::MD5.hexdigest( self.password )
+    self.password = MailboxesHelper::Password.crypt( self.password )
   end
 
   def self.search(search)
