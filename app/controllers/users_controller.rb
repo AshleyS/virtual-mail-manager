24e755ea169d481467e7244c252e44b12eed08aa
Ashley Snowdon <ashley@taikutsu.influentialsoftware.com>
2011-08-05 16:25:26 +0100

    Implemented show/edit/destroy for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,48 @@ class UsersController < ApplicationController
     @users = User.scoped
   end
 
+  def show
+    @user = User.find(params[:id])
+    add_crumb @user.username
+  end
+
   def new
     @user = User.new
+    add_crumb 'New'
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    add_crumb @user.username, user_path(@user)
+    add_crumb 'Edit'
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_path, :notice => "Account created"
+      redirect_to root_path, :notice => "Account was successfully created."
     else
       render "new"
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    add_crumb @user.username, user_path(@user)
+    add_crumb 'Edit'
+
+    if @user.update_attributes(params[:user])
+      redirect_to(@user, :notice => 'Account was successfully updated.')
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to(users_path, :notice => 'User was successfully deleted.')
   end
 
   private
